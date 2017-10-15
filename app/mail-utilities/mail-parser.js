27d199bd0792a84efaf372150d1aeefec247662c
Pablo <pfnieto@uc.cl>
2017-10-15 18:20:27 -0300

    basic listener

---
+++ app/mail-utilities/mail-parser.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parseEmail: function(body, subject, email, name, date) {
+        console.log("Body: " + body)
+        console.log("Subject: " + subject)
+        console.log("Email: " + email)
+        console.log("Name: " + name)
+        console.log("Date: " + date)
+    }
+}
