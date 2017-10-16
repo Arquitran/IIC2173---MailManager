fb7a7bea6b2dd362322d5926a5fe9f938824f598
Andrés Fernández <alfernandez@uc.cl>
2017-10-16 09:57:43 -0300

    linted

--- app/mail-utilities/mail-listener.js
+++ app/mail-utilities/mail-listener.js
@@ -1,44 +1,45 @@
 const mp = require('./mail-parser');
-const axios = require('axios')
-var mailPort = 993
-var MailListener = require("mail-listener2");
+const axios = require('axios');
 
-var mailListener = new MailListener({
-  username: "info.arquitran@gmail.com",
-  password: "hansfindel",
-  host: "imap.gmail.com",
+const mailPort = 993;
+const MailListener = require('mail-listener2');
+
+const mailListener = new MailListener({
+  username: 'info.arquitran@gmail.com',
+  password: 'hansfindel',
+  host: 'imap.gmail.com',
   port: mailPort, // imap port
   tls: true,
   connTimeout: 10000, // Default by node-imap
   authTimeout: 5000, // Default by node-imap,
   debug: null, // Or your custom function with only one incoming argument. Default: null
   tlsOptions: { rejectUnauthorized: false },
-  mailbox: "INBOX", // mailbox to monitor
-  searchFilter: ["UNSEEN"], // the search filter being used after an IDLE notification has been retrieved
+  mailbox: 'INBOX', // mailbox to monitor
+  searchFilter: ['UNSEEN'], // the search filter being used after an IDLE notification has been retrieved
   markSeen: true, // all fetched email willbe marked as seen and not fetched next time
   fetchUnreadOnStart: false, // use it only if you want to get all unread email on lib start. Default is `false`,
-  mailParserOptions: {streamAttachments: false}, // options to be passed to mailParser lib.
+  mailParserOptions: { streamAttachments: false }, // options to be passed to mailParser lib.
   attachments: false, // download attachments as they are encountered to the project directory
-  attachmentOptions: { directory: "attachments/" } // specify a download directory for attachments
+  attachmentOptions: { directory: 'attachments/' }, // specify a download directory for attachments
 });
 
 // stop listening
-//mailListener.stop();
+// mailListener.stop();
 
-mailListener.on("server:connected", function(){
-    console.log("imapConnected");
+mailListener.on('server:connected', () => {
+  console.log('imapConnected');
 });
 
-mailListener.on("server:disconnected", function(){
-    console.log("imapDisconnected");
+mailListener.on('server:disconnected', () => {
+  console.log('imapDisconnected');
 });
 
-mailListener.on("error", function(err){
-    console.log(err);
+mailListener.on('error', (err) => {
+  console.log(err);
 });
 
-mailListener.on("mail", function(mail, seqno, attributes){
-    /*email format is the following
+mailListener.on('mail', (mail, seqno, attributes) => {
+  /* email format is the following
     {
         text: main email body
         subject: email subject
@@ -52,13 +53,13 @@ mailListener.on("mail", function(mail, seqno, attributes){
     }
     there's other stuff but it's metadata/not relevant
     */
-    console.log("Mail received")
-    mp.parseEmail(mail.text, mail.subject, mail.from[0].address, mail.from[0].name, mail.date)
+  console.log('Mail received');
+  mp.parseEmail(mail.text, mail.subject, mail.from[0].address, mail.from[0].name, mail.date);
 });
 
-mailListener.on("attachment", function(attachment){
-    console.log(attachment.path);
+mailListener.on('attachment', (attachment) => {
+  console.log(attachment.path);
 });
 
 mailListener.start();
-console.log("Listening for mails at port " + mailPort);
+console.log(`Listening for mails at port ${mailPort}`);
--- app/mail-utilities/mail-parser.js
+++ app/mail-utilities/mail-parser.js
@@ -1,44 +1,44 @@
-const axios = require('axios')
-//WIP while queue is defined
-const url = 'http://httpbin.org/post'
+const axios = require('axios');
+// WIP while queue is defined
+const url = 'http://httpbin.org/post';
 
 module.exports = {
-    parseEmail: function(body, subject, email, name, date) {
-        //Receive extra information in case it's needed in the future
-        let t0 = body.split("\n")
-        let jsonB
-        for (i = 0; i < t0.length -1; i++) {
-            let tl = t0[i].split(/\s+/)
-            if (tl[0] === "buy") {
-                jsonB = {
-                    action: "buy",
-                    product: tl[1],
-                    ammount: tl[2],
-                    user: email
-                }
-            } else if (tl[0] === "view") {
-                jsonB = {
-                    action: "view",
-                    product: tl[1],
-                    user: email
-                }
-            } else if (tl[0] === "category") {
-                jsonB = {
-                    action: "category",
-                    product: tl[1],
-                    user: email
-                }
-            } else {
-                continue
-            }
-            //Axios is a temporary solution until the queue is chosen
-            axios.post(url, jsonB)
-            .then(function(response) {
-                console.log(response.data)
-            })
-            .catch(function (error) {
-                console.log(error)
-            }) 
-        }
+  parseEmail(body, subject, email, name, date) {
+    // Receive extra information in case it's needed in the future
+    const t0 = body.split('\n');
+    let jsonB;
+    for (i = 0; i < t0.length - 1; i++) {
+      const tl = t0[i].split(/\s+/);
+      if (tl[0] === 'buy') {
+        jsonB = {
+          action: 'buy',
+          product: tl[1],
+          ammount: tl[2],
+          user: email,
+        };
+      } else if (tl[0] === 'view') {
+        jsonB = {
+          action: 'view',
+          product: tl[1],
+          user: email,
+        };
+      } else if (tl[0] === 'category') {
+        jsonB = {
+          action: 'category',
+          product: tl[1],
+          user: email,
+        };
+      } else {
+        continue;
+      }
+      // Axios is a temporary solution until the queue is chosen
+      axios.post(url, jsonB)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-}
+  },
+};
