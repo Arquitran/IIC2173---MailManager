4f5adf875e0b3f7a8a49a8c56c3858bf9db753e5
Andrés Fernández <alfernandez@uc.cl>
2017-10-16 11:15:54 -0300

    linted

--- app/mail-utilities/mailer-sender.js
+++ app/mail-utilities/mailer-sender.js
@@ -1,29 +1,31 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'info.arquitran@gmail.com', // Your email id
-        pass: 'hansfindel' // Your password
-    }
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'info.arquitran@gmail.com', // Your email id
+    pass: 'hansfindel', // Your password
+  },
 });
 
-var text = 'Hello world';
+const text = 'Hello world';
 
-var mailOptions = {
-    from: 'info.arquitran@gmail.com', // sender address
-    to: 'fjvaldes1@uc.cl', // list of receivers
-    subject: 'Email Example', // Subject line
-    text: text //
-    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+const mailOptions = {
+  from: 'info.arquitran@gmail.com', // sender address
+  to: 'fjvaldes1@uc.cl', // list of receivers
+  subject: 'Email Example', // Subject line
+  text, //
+  // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
 };
 
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        console.log(error);
-        //res.json({yo: 'error'});
-    }else{
-        console.log('Message sent: ' + info.response);
-        //res.json({yo: info.response});
-    };
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log(error);
+    // res.json({yo: 'error'});
+  } else {
+    console.log(`Message sent: ${info.response}`);
+    // res.json({yo: info.response});
+  }
 });
+
+module.exports = async function()
