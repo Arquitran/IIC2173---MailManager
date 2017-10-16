82d550921f4c8f7f653385459972dfd436ca81a8
Francisco <fjvaldes1@uc.cl>
2017-10-16 16:41:55 -0300

    mailer respuesta

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
 const ml = require('./app/mail-utilities/mail-listener.js');
 
 const app = express();
@@ -14,6 +16,35 @@ const router = express.Router();
 router.get('/', (req, res) => {
   console.log(req);
   res.json({ message: 'Test' });
+});
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  auth: {
+    user: 'info.arquitran@gmail.com',
+    pass: 'hansfindel',
+  },
+});
+
+router.post('/send',function(req,res){
+    var mailOptions={
+        from: 'info.arquitran@gmail.com',
+        to : req.body.to,
+        subject : 'Respuesta Arquitran',
+        text : req.body.text
+    }
+
+    console.log(mailOptions);
+    transporter.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+    });
 });
 
 app.use('/api', router);
