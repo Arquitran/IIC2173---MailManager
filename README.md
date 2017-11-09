044025cbd39e1ab2596457bb5fa75a576ffcc1b0
sofizambrano <sizambrano@uc.cl>
2017-11-08 22:22:00 -0300

    signUp, signIn and buying transaction

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # IIC2173---MailManager
+
+Commands accepted:
+
+signup [password]
+signin [password]
+buy [productName] [quantity]
+view [productName]
+
+To succesfully buy a product, you must put the signin command before it.
+If you are a new user, you will have to add the signup command before the signin one.
--- app/mail-utilities/mail-parser.js
+++ app/mail-utilities/mail-parser.js
@@ -2,9 +2,11 @@ const sender = require('./mail-sender');
 const http = require('http');
 
 function getResponse(jsonRes) {
-  let baseUrl = process.env.QUEUE_URL || 'http://arqss14.ing.puc.cl';
+  //let baseUrl = process.env.QUEUE_URL || 'http://arqss14.ing.puc.cl';
+  let baseUrl = 'http://arqss17.ing.puc.cl';
   baseUrl += ':3000';
   let url = baseUrl;
+  let apiUrl = 'https://arqss4.ing.puc.cl/api';
   console.log(jsonRes.action);
   switch (jsonRes.action) {
     case 'view':
@@ -14,6 +16,10 @@ function getResponse(jsonRes) {
     case 'category':
       url = `${url}/categorias`;
       break;
+    case 'signup':
+      url = `${apiUrl}/signup`;
+    case 'signin':
+      url = `${apiUrl}/signin`;
     default:
   }
   console.log(url);
@@ -39,6 +45,15 @@ function getResponse(jsonRes) {
 
 module.exports = {
   parseEmail(body, subject, email, name, date) {
+
+    //console.log(body);
+    console.log(body);
+    const u0 = body.split('\n');
+    console.log(u0);
+    const ul = u0[0].split(/\s+/);
+    console.log(ul);
+    console.log(ul[0]);
+    
     // Receive extra information in case it's needed in the future
     const t0 = body.split('\n');
     let jsonB;
@@ -63,10 +78,23 @@ module.exports = {
           product: tl[1],
           user: email,
         };
+      } else if (tl[0] === 'signup') {
+        jsonB = {
+          action: 'signup',
+          password: tl[1],
+          user: email,
+        };
+      } else if (tl[0] === 'signin') {
+        jsonB = {
+          action: 'signin',
+          password: tl[1],
+          user: email,
+        };
       } else {
         sender('Comando inválido', email);
         continue;
       }
+      console.log(jsonB)
       getResponse(jsonB);
     }
   },
